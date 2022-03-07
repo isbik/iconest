0a50fd664b710518d3a16daf370920474dda60be
dmitry <bikishovd>
2022-03-07 17:04:12 +0700

    :sparkles: Update stores, colors, add save to LS, change design a bit

--- frontend/src/components/IconPanel/components/IconColorPicker.tsx
+++ frontend/src/components/IconPanel/components/IconColorPicker.tsx
@@ -1,91 +1,91 @@
-import { useStore } from "effector-react";
-import React, { memo, useState } from "react";
-import { ChromePicker } from "react-color";
+import { useStore } from 'effector-react';
+import React, { memo, useState } from 'react';
+import { ChromePicker } from 'react-color';
 import {
-  $iconEditorStore,
-  $setIconColor,
-  $setIsCurrentColor,
-} from "../../../store/iconEditor";
+	$iconEditorStore,
+	$setIconColor,
+	$setIsCurrentColor,
+} from '../../../store/iconEditor';
 
 type Props = {};
 
 const IconColorPicker = (props: Props) => {
-  const { iconColor, isCurrentColor } = useStore($iconEditorStore);
-  const [displayColorPicker, setDisplayColorPicker] = useState(false);
+	const { iconColor, isCurrentColor } = useStore($iconEditorStore);
+	const [displayColorPicker, setDisplayColorPicker] = useState(false);
 
-  const handleClick = () => {
-    setDisplayColorPicker(!displayColorPicker);
-  };
+	const handleClick = () => {
+		setDisplayColorPicker(!displayColorPicker);
+	};
 
-  const handleClose = () => {
-    setDisplayColorPicker(false);
-  };
+	const handleClose = () => {
+		setDisplayColorPicker(false);
+	};
 
-  const handleChangeColor = (color: string) => {
-    if (color === iconColor || color === "currentColor") return;
-    $setIconColor(color);
-  };
+	const handleChangeColor = (color: string) => {
+		if (color === iconColor || color === 'currentColor') return;
+		$setIconColor(color);
+	};
 
-  const handleChange = (v: { hex: string }) => handleChangeColor(v.hex);
+	const handleChange = (v: { hex: string }) => handleChangeColor(v.hex);
 
-  const handleCurrentColorChecked = () => {
-    handleChangeColor("currentColor");
-    $setIsCurrentColor(!isCurrentColor);
-  };
+	const handleCurrentColorChecked = () => {
+		handleChangeColor('currentColor');
+		$setIsCurrentColor(!isCurrentColor);
+	};
 
-  return (
-    <div className="flex items-center mb-4">
-      <label className="mr-2" htmlFor="color">
-        Current color
-      </label>
-      <input
-        checked={isCurrentColor}
-        className="mr-auto"
-        id="color"
-        type="checkbox"
-        onChange={handleCurrentColorChecked}
-      />
-      <button
-        disabled={isCurrentColor}
-        className="flex items-center h-6 rounded-md disabled:opacity-50 border-slate-50 bg-slate-50 text-slate-900"
-        onClick={handleClick}
-      >
-        <span className="px-1 pl-2 mr-1">{iconColor}</span>
-        <div
-          className="w-6 rounded aspect-square"
-          style={{
-            background: iconColor || "bg-slate-500",
-          }}
-        ></div>
-      </button>
+	return (
+		<div className="flex items-center mb-4">
+			<label className="mr-2" htmlFor="color">
+				Current color
+			</label>
+			<input
+				checked={isCurrentColor}
+				className="mr-auto"
+				id="color"
+				type="checkbox"
+				onChange={handleCurrentColorChecked}
+			/>
+			<button
+				disabled={isCurrentColor}
+				className="flex items-center h-6 rounded-md disabled:opacity-50 border-slate-50 bg-slate-50 text-slate-900"
+				onClick={handleClick}
+			>
+				<span className="px-1 pl-2 mr-1">{iconColor}</span>
+				<div
+					className="w-6 rounded aspect-square"
+					style={{
+						background: iconColor || 'bg-slate-500',
+					}}
+				></div>
+			</button>
 
-      {displayColorPicker ? (
-        <div
-          style={{
-            position: "absolute",
-            zIndex: "2",
-          }}
-        >
-          <div
-            style={{
-              position: "fixed",
-              top: "0px",
-              right: "0px",
-              bottom: "0px",
-              left: "0px",
-            }}
-            onClick={handleClose}
-          />
-          <ChromePicker
-            disableAlpha
-            className="color-picker"
-            color={iconColor}
-            onChange={handleChange}
-          />
-        </div>
-      ) : null}
-    </div>
-  );
+			{displayColorPicker ? (
+				<div
+					style={{
+						position: 'absolute',
+						zIndex: '2',
+					}}
+				>
+					<div
+						style={{
+							position: 'fixed',
+							top: '0px',
+							right: '0px',
+							bottom: '0px',
+							left: '0px',
+						}}
+						onClick={handleClose}
+					/>
+					<ChromePicker
+						disableAlpha
+						className="color-picker"
+						color={iconColor}
+						onChange={handleChange}
+					/>
+				</div>
+			) : null}
+		</div>
+	);
 };
 
 export default memo(IconColorPicker);
--- frontend/src/components/IconPanel/components/IconViewSettings.tsx
+++ frontend/src/components/IconPanel/components/IconViewSettings.tsx
@@ -1,112 +1,112 @@
-import { useStore } from "effector-react";
-import React, { memo } from "react";
+import { useStore } from 'effector-react';
+import React, { memo } from 'react';
 import {
-  $iconEditorStore,
-  $resetIconEditorStore,
-  $selectedIconStore,
-  $setIcon,
-  $setIconSize,
-  $setIsInheritSize,
-} from "../../../store/iconEditor";
-import IconColorPicker from "./IconColorPicker";
+	$iconEditorStore,
+	$resetIconEditorStore,
+	$selectedIconStore,
+	$setIcon,
+	$setIconSize,
+	$setIsInheritSize,
+} from '../../../store/iconEditor';
+import IconColorPicker from './IconColorPicker';
 
 type Props = {
-  svg: string;
+	svg: string;
 };
 
 const IconViewSettings = ({ svg }: Props) => {
-  const { iconSize, isInheritSize } = useStore($iconEditorStore);
-  const selectedIcon = useStore($selectedIconStore);
+	const { iconSize, isInheritSize } = useStore($iconEditorStore);
+	const selectedIcon = useStore($selectedIconStore);
 
-  if (!selectedIcon) return null;
+	if (!selectedIcon) return null;
 
-  const handleReset = () => {
-    const icon = { ...selectedIcon };
+	const handleReset = () => {
+		const icon = { ...selectedIcon };
 
-    $resetIconEditorStore();
+		$resetIconEditorStore();
 
-    $setIcon(icon);
-  };
+		$setIcon(icon);
+	};
 
-  return (
-    <>
-      <div className="flex items-center mb-4 text-slate-100">
-        <p className="flex overflow-hidden font-bold grow text-ellipsis whitespace-nowrap">
-          {selectedIcon.name}
-        </p>
-        <button
-          onClick={handleReset}
-          className="mr-2 text-white rounded hover:text-sky-500"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="22"
-            height="22"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
-            />
-            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />{" "}
-          </svg>
-        </button>
-        <button
-          onClick={() => $setIcon(null)}
-          className="rounded hover:text-sky-500 text-slate-100"
-        >
-          <svg
-            viewBox="0 0 24 24"
-            width="25"
-            height="25"
-            stroke="currentColor"
-            strokeWidth="2"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="18" y1="6" x2="6" y2="18"></line>
-            <line x1="6" y1="6" x2="18" y2="18"></line>
-          </svg>
-        </button>
-      </div>
-      <div
-        key={selectedIcon.name}
-        dangerouslySetInnerHTML={{
-          __html: svg,
-        }}
-        className="text-3xl flex items-center justify-center w-full mb-4 rounded min-h-[8rem] bg-slate-200 text-slate-900"
-      ></div>
-      <div className="flex items-center mb-2">
-        <label className="mr-2" htmlFor="color">
-          Inherit size
-        </label>
-        <input
-          checked={isInheritSize}
-          className="mr-auto"
-          id="color"
-          type="checkbox"
-          onChange={() => $setIsInheritSize(!isInheritSize)}
-        />
-        <span className="mx-1 text-xs grow">
-          {isInheritSize ? "1em" : iconSize + "px"}
-        </span>
-        <input
-          disabled={isInheritSize}
-          value={iconSize}
-          className="mr-auto disabled:opacity-10"
-          onChange={(event) => $setIconSize(+event.target.value)}
-          type="range"
-          id="Size"
-          name="Size"
-          min="14"
-          max="100"
-        />
-      </div>
-      <IconColorPicker />
-    </>
-  );
+	return (
+		<>
+			<div className="flex items-center mb-4 text-slate-100">
+				<p className="flex overflow-hidden font-bold grow text-ellipsis whitespace-nowrap">
+					{selectedIcon.name}
+				</p>
+				<button
+					onClick={handleReset}
+					className="mr-2 text-white rounded hover:text-sky-500"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="22"
+						height="22"
+						fill="currentColor"
+						viewBox="0 0 16 16"
+					>
+						<path
+							fillRule="evenodd"
+							d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
+						/>
+						<path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />{' '}
+					</svg>
+				</button>
+				<button
+					onClick={() => $setIcon(null)}
+					className="rounded hover:text-sky-500 text-slate-100"
+				>
+					<svg
+						viewBox="0 0 24 24"
+						width="25"
+						height="25"
+						stroke="currentColor"
+						strokeWidth="2"
+						fill="none"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					>
+						<line x1="18" y1="6" x2="6" y2="18"></line>
+						<line x1="6" y1="6" x2="18" y2="18"></line>
+					</svg>
+				</button>
+			</div>
+			<div
+				key={selectedIcon.name}
+				dangerouslySetInnerHTML={{
+					__html: svg,
+				}}
+				className="text-3xl flex items-center justify-center w-full mb-4 rounded min-h-[8rem] bg-slate-200 text-slate-900"
+			></div>
+			<div className="flex items-center mb-2">
+				<label className="mr-2" htmlFor="color">
+					Inherit size
+				</label>
+				<input
+					checked={isInheritSize}
+					className="mr-auto"
+					id="color"
+					type="checkbox"
+					onChange={() => $setIsInheritSize(!isInheritSize)}
+				/>
+				<span className="mx-1 text-xs grow">
+					{isInheritSize ? '1em' : iconSize + 'px'}
+				</span>
+				<input
+					disabled={isInheritSize}
+					value={iconSize}
+					className="mr-auto disabled:opacity-10"
+					onChange={(event) => $setIconSize(+event.target.value)}
+					type="range"
+					id="Size"
+					name="Size"
+					min="14"
+					max="100"
+				/>
+			</div>
+			<IconColorPicker />
+		</>
+	);
 };
 
 export default memo(IconViewSettings);
